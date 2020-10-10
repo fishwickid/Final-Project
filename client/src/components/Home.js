0fad3eeec249c018d72740036a9a91cdd6573aef
fishwickid <chadfishwick@gmail.com>
2020-10-10 16:03:20 +0800

    Home Template

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
-export  function Home() {
-
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+import React from "react";
+export function Home() {
+    
+  return (
+    <div>
+      <h1>Guitar Jam Connect</h1>
+      <h3>Connect with people who want to improve their playing skills</h3>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,16 +1,26 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../lib";
+
 export function Navigation() {
-    return(
-        <nav>
-            <ul style={{display: "flex", listStyle: "none"}}>
-                <li style={{margin: "0 1em"}}><Link to="/">Home</Link></li>
-                <li style={{margin: "0 1em"}}><Link to="/signup">Sign Up</Link></li>
-                <li style={{margin: "0 1em"}}><Link to="/login">Log In</Link></li>
-                <li style={{margin: "0 1em"}}><Link to="/logout">Log Out</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul style={{ display: "flex", listStyle: "none" }}>
+        <li style={{ margin: "0 1em" }}>
+          <Link to="/">Home</Link>
+        </li>
+        <li style={{ margin: "0 1em" }}>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li style={{ margin: "0 1em" }}>
+          <Link to="/login">Log In</Link>
+        </li>
+        <li style={{ margin: "0 1em" }}>
+          <Link to="/logout">Log Out</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
