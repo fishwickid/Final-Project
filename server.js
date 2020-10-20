788cf9345fcda200e6cf59615a64794b90546dc3
fishwickid <chadfishwick@gmail.com>
2020-10-20 18:00:41 +0800

    new server info

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ server.use(passport.initialize());
 server.use("/api", router);
 server.use("/api", jamsRouter);
 
-if (process.env.NODE_ENV === "production") {
-  server.use(express.static("client/build"));
+server.use(express.static(__dirname + "/public"));
 
-  server.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   server.use(express.static("client/build"));
+
+//   server.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build/index.html"));
+//   });
+// }
 
 // router.use(function (req, res) {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
@@ -33,6 +35,20 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jamsesh", {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+if ((process.env.NODE_ENV = "production")) {
+  server.use(express.static("client/build"));
+  server.use("*", express.static("client/build"));
+  server.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"), function (
+      err
+    ) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
